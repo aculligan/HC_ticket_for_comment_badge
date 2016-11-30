24d69d8b388b390b34ce76034155fff4814d5ba6
Alex Culligan <aculligan@users.noreply.github.com>
2016-11-30 17:10:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 // set ticket badge color by status
 $(document).ready(function() {
-  const subdomain = 'SUBDOMAIN'; //replace SUBDOMAIN for account subdomain. For example: var subdomain = 'support';
+  // Replace SUBDOMAIN for account subdomain. For example: const subdomain = 'support';
+  const subdomain = 'z3nculligan';
   const userRole = HelpCenter.user.role;
-  const $thisItem = $(this);
   if (userRole == 'agent' || userRole == 'manager') {
-    const $postComment = $('.comment');
-    const $ticketBadge = $('#comments li .escalation-badge');
+    let $postComment = $('.comment');
+    let $ticketBadge = $('#comments li .escalation-badge');
     $ticketBadge.attr('id', function(i) {
       return 'comment_badge'+(i+1);
     });
     $postComment.each(function() {
-      const $ticketBadgeID = $thisItem.find($ticketBadge).attr('id');
-      const $ticketURL = $thisItem.find($ticketBadge).attr('href');
+      let $ticketBadgeID = $(this).find($ticketBadge).attr('id');
+      let $ticketURL = $(this).find($ticketBadge).attr('href');
       if ($ticketURL != undefined) {
-        const $urlSplit = $ticketURL.split('/tickets/');
-        const $ticketID = $urlSplit[1];
-        const apiURL = `https://$(subdomain).zendesk.com/api/v2/tickets/$($ticketID).json`;
-        const getData = $.ajax({
+        let $urlSplit = $ticketURL.split('/tickets/');
+        let $ticketID = $urlSplit[1];
+        let apiURL = `https://${subdomain}.zendesk.com/api/v2/tickets/${$ticketID}.json`;
+        let getData = $.ajax({
           type: 'GET',
           url: apiURL,
           dataType: 'json'
         });
         getData.success(function(data){
-          const ticketStatus = data.ticket.status;
-          const $ticketBadgeIDElement = $('#' + $ticketBadgeID);
+          let ticketStatus = data.ticket.status;
+          let $ticketBadgeIDElement = $('#' + $ticketBadgeID);
           if (ticketStatus == 'new') {
             $ticketBadgeIDElement
               .css({'background': '#F5CA00', 'border-color': '#F5CA00'})
-              .text(`Ticket #$($ticketID) - New`);
+              .text(`Ticket #${$ticketID} - New`);
           }
           if (ticketStatus == 'open') {
             $ticketBadgeIDElement
               .css({'background': '#E82A2A', 'border-color': '#E82A2A'})
-              .text(`Ticket #$($ticketID) - Open`);
+              .text(`Ticket #${$ticketID} - Open`);
           }
           if (ticketStatus == 'pending') {
             $ticketBadgeIDElement
               .css({'background': '#59BBE0', 'border-color': '#59BBE0'})
-              .text(`Ticket #$($ticketID) - Pending`);
+              .text(`Ticket #${$ticketID} - Pending`);
           }
           if (ticketStatus == 'hold') {
             $ticketBadgeIDElement
               .css({'background': '#000000', 'border-color': '#000000'})
-              .text(`Ticket #$($ticketID) - On-Hold`);
+              .text(`Ticket #${$ticketID} - On-Hold`);
           }
           if (ticketStatus == 'solved' || ticketStatus == 'closed') {
             $ticketBadgeIDElement
               .css({'background': '#828282', 'border-color': '#828282'})
-              .text(`Ticket #$($ticketID) - Solved`);
+              .text(`Ticket #${$ticketID} - Solved`);
           }
         })
       }
